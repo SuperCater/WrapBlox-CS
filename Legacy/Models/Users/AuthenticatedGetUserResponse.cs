ef4959df5ba0b0e63a3b1ddc4ee3ed86110c12ae
SuperCater <supercater500@gmail.com>
2025-07-26 18:46:31 -0400

    doc AuthenticatedGetUserResponse

--- Legacy/Models/Users/AuthenticatedGetUserResponse.cs
+++ Legacy/Models/Users/AuthenticatedGetUserResponse.cs
@@ -2,13 +2,27 @@ using Newtonsoft.Json;
 
 namespace WrapBlox.Legacy.Models.Users;
 
+/// <summary>
+/// A response model representing absolute minimal authenticating user information.
+/// No new attributes should be added to this response since it is in the critical path of app launch and we want to minimize dependencies.
+/// 
+
 public record AuthenticatedGetUserResponse {
+	/// <summary>
+	/// The user's ID.
+	/// </summary>
 	[JsonProperty("id")]
 	public required ulong Id { get; init; }
 	
+	/// <summary>
+	/// The user's name.
+	/// </summary>
 	[JsonProperty("name")]
 	public required string Name { get; init; }
 	
+	/// <summary>
+	/// The user's display name.
+	/// </summary>
 	[JsonProperty("displayName")]
 	public required string DisplayName { get; init; }
 }
