a403216fe99df6452267a95dbfe7b9963fd2f61f
Purple_Creativity <thepurplecreativity@gmail.com>
2025-07-27 00:27:14 +0200

    ApiPageResponse

---
+++ Legacy/Models/ApiPageResponse.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace WrapBlox.Legacy.Models;
+
+public record ApiPageResponse<T>(
+    [JsonProperty("previousPageCursor")]
+    string PreviousPageCursor,
+    
+    [JsonProperty("nextPageCursor")]
+    string NextPageCursor,
+    
+    [JsonProperty("data")]
+    T[] Data
+);
