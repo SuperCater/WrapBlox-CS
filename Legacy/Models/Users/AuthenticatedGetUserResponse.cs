74c0f0a6c074323b0cf4bb3c26240b0c453f8923
Purple_Creativity <113302593+PurpleCreativity@users.noreply.github.com>
2025-07-27 12:29:22 +0200

    Update AuthenticatedGetUserResponse.cs

--- Legacy/Models/Users/AuthenticatedGetUserResponse.cs
+++ Legacy/Models/Users/AuthenticatedGetUserResponse.cs
@@ -4,7 +4,6 @@ namespace WrapBlox.Legacy.Models.Users;
 
 /// <summary>
 /// A response model representing absolute minimal authenticating user information.
-/// No new attributes should be added to this response since it is in the critical path of app launch and we want to minimize dependencies.
 /// </summary>
 public record AuthenticatedGetUserResponse {
 	/// <summary>
