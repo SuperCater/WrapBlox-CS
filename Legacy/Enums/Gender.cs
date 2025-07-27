0237fee556a7258ec575c8359ad1d2c48bc22cda
SuperCater <supercater500@gmail.com>
2025-07-26 23:57:53 -0400

    I walk on water

---
+++ Legacy/Enums/Gender.cs
@@ -0,0 +1,7 @@
+namespace WrapBlox.Legacy.Enums;
+
+public enum Gender {
+	None = 1,
+	Male = 2,
+	Female = 3,
+}
---
+++ Legacy/Models/Users/GenderRequest.cs
@@ -0,0 +1,14 @@
+using Newtonsoft.Json;
+
+namespace WrapBlox.Legacy.Models.Users;
+
+/// <summary>
+/// The request to change a user's set gender. 🏳️‍⚧️
+/// </summary>
+public record GenderRequest {
+	/// <summary>
+	/// The user's gender.
+	/// </summary>
+	[JsonProperty("gender")]
+	public required string Gender { get; init; }
+}
