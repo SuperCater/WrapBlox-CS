07706d875fee1f96a83b0898a035bd1051022f69
SuperCater <supercater500@gmail.com>
2025-07-27 00:07:40 -0400

    Why are expectations so high? Is it the bar I set?

---
+++ Legacy/Models/Users/MultiGetByUserIdRequest.cs
@@ -0,0 +1,20 @@
+using Newtonsoft.Json;
+
+namespace WrapBlox.Legacy.Models.Users;
+
+/// <summary>
+/// Request model for getting users by ids.
+/// </summary>
+public record MultiGetByUserIdRequest {
+	/// <summary>
+	/// The user IDs to get the users for.
+	/// </summary>
+	[JsonProperty("userIds")]
+	public required ulong[] UserIds { get; init; }
+	
+	/// <summary>
+	/// If true, the request will not return users that are banned.
+	/// </summary>
+	[JsonProperty("excludeBannedUsers", NullValueHandling = NullValueHandling.Ignore)]
+	public bool? ExcludeBannedUsers { get; init; } = true;
+};
