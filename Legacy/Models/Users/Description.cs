e7f6fedb4998ae70d82500d9a083b0160207984a
Purple_Creativity <113302593+PurpleCreativity@users.noreply.github.com>
2025-07-27 20:18:15 +0200

    Update Description.cs

--- Legacy/Models/Users/Description.cs
+++ Legacy/Models/Users/Description.cs
@@ -2,9 +2,9 @@ using Newtonsoft.Json;
 
 namespace WrapBlox.Legacy.Models.Users;
 
-// This is the request and response model for the description of a user. They are the same.
 /// <summary>
 /// The request and response model for the description of a user.
+/// <remarks>This is the request and response model for the description of a user. They are the same.</remarks>
 /// </summary>
 public record DescriptionRequest {
 	/// <summary>
