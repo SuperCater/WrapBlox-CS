782bf3a6beaab51408f1f3034a5160e2aca26978
SuperCater <supercater500@gmail.com>
2025-07-26 20:27:42 -0400

    BirthdateRequest model

--- Legacy/Models/Users/BirthdateRequest.cs
+++ Legacy/Models/Users/BirthdateRequest.cs
@@ -1,3 +1,29 @@
+using Newtonsoft.Json;
+
 namespace WrapBlox.Legacy.Models.Users;
 
-public record BirthdataRequest();
+/// <summary>
+/// Used to upload a user's birthdate for Roblox.
+/// </summary>
+public record BirthdateRequest {
+	/// <summary>
+	/// The user's birth month.
+	/// </summary>
+	[JsonProperty("birthMonth")]
+	public required int BirthMonth { get; init; }
+	/// <summary>
+	/// The user's birth day of the month.
+	/// </summary>
+	[JsonProperty("birthDay")]
+	public required int BirthDay { get; init; }
+	/// <summary>
+	/// The user's birth year.
+	///	</summary>
+	[JsonProperty("birthYear")]
+	public required int BirthYear { get; init; }
+	/// <summary>
+	/// The user's password. This is a security requirement by Roblox.
+	/// </summary>
+	[JsonProperty("password")]
+	public required string Password { get; init; }
+}
