324f0941fddd60293b45b5b8ba13a902e1afc9a5
SuperCater <supercater500@gmail.com>
2025-07-27 00:16:20 -0400

    I lose my grip like the flying trapeze

--- Legacy/Models/Users/MultiGetUserByNameResponse.cs
+++ Legacy/Models/Users/MultiGetUserByNameResponse.cs
@@ -5,18 +5,10 @@ namespace WrapBlox.Legacy.Models.Users;
 /// <summary>
 /// A response model specific to multi-get user by name.
 /// </summary>
-public record MultiGetUserByNameResponse : AuthenticatedGetUserResponse {
+public record MultiGetUserByNameResponse : MultiGetUserResponse {
 	/// <summary>
 	/// The username used for the request.
 	/// </summary>
 	[JsonProperty("requestedUsername")]
 	public required string RequestedUsername { get; init; }
-	
-	/// <summary>
-	/// The user's verified badge status.
-	/// </summary>
-	[JsonProperty("hasVerifiedBadge")]
-	public required bool HasVerifiedBadge { get; init; }
-	
-	
 };
